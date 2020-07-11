4047932ffa4391ddaac8de6d11f91615c09eeec5
Dexter Bigueta <dbigueta@sfu.ca>
2020-07-10 18:27:37 -0700

    Created the react app, deleted what was not needed and created the skeleton of the main App.js file

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+
+
+
+/*
+function someFunction() {
+
+
+Functions like figuring out the infinite scroll and input output will be above the App function
+
+}
+*/
+
+
+
+// This is the main code below, this of the App function as your int main()
+// Everything above is going to be your functions, think of them like prototyping your functions
+
+function App() {
+  return (
+    <div> {/* Between this div is the main div container, everything in here will be the website */}
+
+        <div> {/* Between this div is where you're going to call the scrolling function */}
+
+          <div> {/* Between this div is where the background image is going to go */}
+
+            <img className="background-image" src="./Hehehe.jpg" alt=""/>            
+
+            <div> {/* Between this div is where the text prompts are going to be */}
+              
+            </div>
+
+            <div> {/* Between this div is where visual animations are going to be (Like the rocket ship and the person travelling) */}
+
+            </div>
+
+            <div> {/* Between this div is where any audio is going to go */}
+
+            </div>
+
+          </div>
+
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
