23a8c9d2a3b4fc03f8a4b1271e5419deea6eefcd
Dexter Bigueta <dbigueta@sfu.ca>
2020-09-10 19:02:15 -0700

    Added text

--- src/App.js
+++ src/App.js
@@ -12,13 +12,15 @@ function App() {
       </audio> */}
 
       <Parallax speed={-1}>
+
         <div className="page1">
+
           <div className="pictureDiv">
             <Parallax speed={-2}>
               <img className="clouds" src="./pages/Page1/Clouds.svg" alt=""/>
             </Parallax>
             <img className="mountains" src="./pages/Page1/Mountains.svg" alt=""/>
-            
+
             <div className="mountainDiv1">
               <Parallax speed={-3}>
                 <img className="sun" src="./pages/Page1/Sun.svg" alt=""/>
@@ -26,11 +28,17 @@ function App() {
               <img className="boyMC1" src="./pages/Page1/BoyMC.svg" alt=""/>
             </div>
           </div>
+
+          <div className="textDiv">
+            <p class="text">Ever since Kay was a baby</p>
+          </div>
         </div>
       </Parallax>
 
       <Parallax speed={-1}>
+
         <div className="page2">
+
           <div className="pictureDiv">
             <Parallax speed={-1}>
               <img className="stars" src="./pages/Page2/Stars.svg" alt=""/>
@@ -38,11 +46,16 @@ function App() {
             <img className="mountains" src="./pages/Page2/Mountains.svg" alt=""/>
             
             <div className="mountainDiv2">
+
               <Parallax speed={-2}>
                 <img className="moon" src="./pages/Page2/Moon.svg" alt=""/>
               </Parallax>
               <img className="boyMC2" src="./pages/Page2/BoyMC.svg" alt=""/>
             </div>
+          </div>
+
+          <div className="textDiv">
+            <p class="text">Everyone told her to shoot for the stars</p>
           </div>
         </div>
       </Parallax>
